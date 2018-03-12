98548874f219aa03be42ce6eaf70e080d6139ea5
ZhihaoDengDeng <985163829@qq.com>
2018-03-13 00:54:24 +0800

    First time to commit project.(ZhiHaoDengDeng)

---
+++ src/com/scau/address/bean/TextForPush.java
@@ -0,0 +1,10 @@
+package com.scau.address.bean;
+
+public class TextForPush {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("hey guys!");
+	}
+
+}
